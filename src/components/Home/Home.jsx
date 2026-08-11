d269a1ade5c55a15478ff608df7f0cfec48f3665
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2026-08-12 01:44:46 +0530

    test pipeline with new application

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,9 @@ const Home = () => {
     <div className="main_container">
       <main className="home">
         <section className="left_section">
-          <h1>What is this catalogue website is all about created by Noman?</h1>
+          <h4>What is this catalogue website is all about created by Noman</h4>
           <br />
+          <i>THIS WAS A CI-CD PRACTICE WITH AWS CODE BUILD AND CODE PIPELINE</i>
         </section>
         <section className="right_section">
           <img src="./test.jpg" alt="" />
